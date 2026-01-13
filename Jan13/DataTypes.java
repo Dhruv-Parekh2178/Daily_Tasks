e9254a3185a703ea0a9dca677451efa5cf8360c5
Dhruv-Parekh2178 <dhruvparekh1229@gmail.com>
2026-01-13 11:50:39 +0530

    Add DataTypes file

---
+++ Jan13/DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num1=9;    //4byte
+        byte by=127;    //1byte -> -2^7 to 2^7-1
+        short sh=558;  // 2byte
+        long l=5854l;  //8byte
+
+        float f=5.8f;  //4byte
+        double d=5.8;  //8byte(default)
+
+        char c='k';  // 2byte  work with UNICODE
+
+        boolean b=true;
+
+    }
+}
