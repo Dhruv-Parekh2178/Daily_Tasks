cbe05511d17881964d65bbac8c02ac8e087b8eb4
Dhruv-Parekh2178 <dhruvparekh1229@gmail.com>
2026-01-13 11:43:48 +0530

    Add Hello file

---
+++ Jan13/Hello.java
@@ -0,0 +1,19 @@
+//  this is the single line comment
+/* this is multi line comment in java */
+
+import java.util.Scanner;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        //simple java output
+        System.out.println("Hello World");
+       //Simple java input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int num = sc.nextInt();
+//      same as we take input for all the primitive datatye use methods like nextByte(),nextFloat(),nextDouble() etc.
+
+
+    }
+}
