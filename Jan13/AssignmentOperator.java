f0a1b9e502ee8057cab161314214d94a04b54334
Dhruv-Parekh2178 <dhruvparekh1229@gmail.com>
2026-01-13 11:54:32 +0530

    Add AssignmentOperator file

---
+++ Jan13/AssignmentOperator.java
@@ -0,0 +1,33 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+    // addition operator "+"
+/**		int num1=7;
+		int num2=5;
+		int result=num1+num2;
+		System.out.println(result);
+**/	
+		//substraction operator '-'
+        //multiplication operator '*'
+        //Division operator '/'
+        //modulo operator '%'
+		
+		int num=7;
+
+//		num=num+2;
+//		num+=2;
+//		num*=2;
+		
+		num++;    //post increment
+		++num;    //pre increment
+		num--;    //post decrement
+		--num;	  //pre decrement
+		System.out.println(num);
+		
+		int result=num++; //fetch the value and then increment
+		// int result=++num++; // first increment and then fetch the value 
+		System.out.println(result);		
+	
+	}
+
+}
